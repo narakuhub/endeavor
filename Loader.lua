5b41e3f4a8c98c9830b1d764dfaa2c6babd0bf8d
narakuhub <narakuhub@gmail.com>
2026-08-03 12:47:10 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,18 @@
+local BYPASS_URL = "https://raw.githubusercontent.com/narakuhub/vetrou/refs/heads/main/Byppas.lua"
+local SOURCE_URL = "https://raw.githubusercontent.com/narakuhub/vetrou/refs/heads/main/source.lua"
 
+pcall(function()
+	local bypassCode = game:HttpGet(BYPASS_URL)
+	if bypassCode and bypassCode ~= "" then
+		loadstring(bypassCode)()
+	end
+end)
+
+task.wait(2)
+
+pcall(function()
+	local sourceCode = game:HttpGet(SOURCE_URL)
+	if sourceCode and sourceCode ~= "" then
+		loadstring(sourceCode)()
+	end
+end)
